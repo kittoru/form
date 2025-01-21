cc5e73353e83a853d345f87782964be3b8447fab
Kittoru <mama190774@gmail.com>
2025-01-21 16:35:47 +0300

    frat: add export

---
+++ src/Shared/index.ts
@@ -0,0 +1,8 @@
+export { Input } from './ui/Input/Input';
+export { Continue } from './ui/Continue/Continue';
+export { Password } from './ui/Password/Password';
+export { Error } from './ui/Error/Error';
+export { Email } from './ui/Email/Email';
+export { Top } from './ui/Top/Top';
+export { Bottom } from './ui/Bottom/Bottom';
+export { Submit } from './ui/Submit/Submit';
