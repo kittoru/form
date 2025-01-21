1d5cc930d72f622a2754683dfc116329c6e2f15d
Kittoru <mama190774@gmail.com>
2025-01-21 16:30:47 +0300

    fix: change input for password

--- src/Shared/ui/Password/Password.tsx
+++ src/Shared/ui/Password/Password.tsx
@@ -1,13 +1,22 @@
 import { useState } from 'react';
+import { Path, UseFormRegister, UseFormWatch, FieldErrors } from 'react-hook-form';
+import { Error } from '../Error/Error';
+import { Inputs } from '../../type/type';
+
 import styles from './Password.module.scss';
+import Visible from '../assets/visible.svg';
+import Invisible from '../assets/invisible.svg';
 
 type Props = {
-  id: string;
-  name: string;
-  label: string;
-  minlength?: number;
+  label: Path<Inputs>;
+  minlength: number;
+  confirm?: boolean;
+  register: UseFormRegister<Inputs>;
+  watch: UseFormWatch<Inputs>;
+  errors: FieldErrors<Inputs>;
 }
-export const Password = ({id, name, label, minlength}: Props) => {
+
+export const Password = ({label, minlength, register, watch, errors, confirm }: Props) => {
   const [type, setType] = useState<string>('password');
   const changeType = () => {
     if (type === 'password') {
@@ -18,10 +27,33 @@ export const Password = ({id, name, label, minlength}: Props) => {
   }
   return ( 
     <div className={styles.block}>
-      <label htmlFor={id} className={styles.lable}>{label}</label>
-      <input id={id} name={name} type={type} required={true} minLength={minlength} className={styles.input}/>
-      <div className={styles.view} onClick={changeType}></div>
-      <a href=""></a>
+      <label className={styles.label}>{label}</label>
+      <div className={styles.content}>
+        <input type={type} className={`${errors[label]? styles.invalid: ''} ${watch(label)? styles.valid: ''}`} {...register(label, 
+        { required: "The field is not filled in!", 
+          minLength: { 
+            value: minlength, 
+            message: 'Minimum of 8 characters!'
+          }, 
+          pattern: {
+            value: /^[A-Z0-9]/i,
+            message: 'Password only eng and numbers'
+          },
+          validate: (val:string) => {
+            if(confirm) {
+              if (watch('Password') != val) {
+                return "Your passwords do no match";
+              }
+            }
+          }
+        })}/>
+        <div className={styles.view} onClick={changeType}>
+          <img src={type==='text'? Invisible: Visible} alt="visible" />
+        </div>
+      </div>
+      <div className={styles.error}>
+        {errors[label] && <Error text={errors[label]?.message || 'Error!'}/>}         
+      </div>
     </div>
   );
 }
