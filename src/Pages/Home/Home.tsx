1cc62cb6dd870b6efa33ea5a2cfacd15caea037c
Kittoru <mama190774@gmail.com>
2025-01-21 18:25:54 +0300

    feat: add logic for pages

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,15 +1,14 @@
+import { useContext } from 'react';
 import { LogIn, SignUp } from '../../Widgets';
+import { PageContext } from '../../app/context/Context';
 
 import styles from './Home.module.scss';
 
-type Props = {
- 
-}
-export const Home = ({}: Props) => {
+export const Home = () => {
+  const page = useContext(PageContext);
   return ( 
-    <div className='container'>
-      {/* <LogIn/> */}
-      <SignUp/>
-    </div>
+      <div className='container'>
+        {page.page === 'login'? <LogIn/>: <SignUp/>}
+      </div>
   );
 }
